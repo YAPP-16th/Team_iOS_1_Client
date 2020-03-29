82e69a34706b2d1ac76941f04df5318404469cb1
ByeonggeunSon <qudrms007@naver.com>
2020-03-29 15:54:39 +0900

    feat: 위치기반 동의 기본 로직 추가 - ARC 특성상 instance를 didFinishLaunching에서만 사용할 경우 사용자의 리액션보다 메모리해제가 더 빨라 동의 전 alert가 사라짐

--- GotGam/GotGam/AppDelegate.swift
+++ GotGam/GotGam/AppDelegate.swift
@@ -7,16 +7,41 @@
 //
 
 import UIKit
+import CoreLocation
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
 
   var window: UIWindow?
-
+  var locationManager: CLLocationManager?
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    locationManager = CLLocationManager()
+    locationManager?.delegate = self
+    locationManager?.requestWhenInUseAuthorization()
+//    checkLaunchWithLocationUpdates(options: launchOptions)
     return true
   }
 
+  func checkLaunchWithLocationUpdates(options: [UIApplication.LaunchOptionsKey : Any]?){
+    if let options = options, let isLocation = options[UIApplication.LaunchOptionsKey.location] as? Bool {
+      if isLocation{
+        let manager = CLLocationManager()
+        manager.delegate = self
+        // 위치 기반으로 실행 될시 처리할 로직 추가
+      }
+    }
+  }
+  
+  func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+    switch status{
+    case .authorizedAlways, .authorizedWhenInUse:
+      print("Authorized")
+    case .denied, .notDetermined, .restricted:
+      print("UnAuthorized")
+    @unknown default:
+      break
+    }
+  }
 }
 
