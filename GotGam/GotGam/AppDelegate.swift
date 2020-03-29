22a4f4cdbf67e2499b37328c7cf4ded6dbe84892
ByeonggeunSon <qudrms007@naver.com>
2020-03-29 14:40:35 +0900

    기본 프로젝트 생성 및 의존성 추가

---
+++ GotGam/GotGam/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  GotGam
+//
+//  Created by byeonggeunSon on 2020/03/29.
+//  Copyright © 2020 손병근. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+
+}
+
---
+++ GotGam/GotGam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GotGam
+//
+//  Created by byeonggeunSon on 2020/03/29.
+//  Copyright © 2020 손병근. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
