2886f05dd289ef65a5fc56a8b5be1cb2f7900301
ByeonggeunSon <qudrms007@naver.com>
2020-04-11 13:43:26 +0900

    feat: 핀, 서클 등 특정 기능 테스트중

--- GotGam/GotGam/Sources/ViewControllers/MapViewController.swift
+++ GotGam/GotGam/Sources/ViewControllers/MapViewController.swift
@@ -19,26 +19,59 @@ class MapViewController: BaseViewController, ViewModelBindableType, MTMapViewDel
     // MARK: - Views
     
     @IBOutlet var mapView: MTMapView!
-    
+    var poiItem1: MTMapPOIItem!
     // MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureMapView()
+        setSomePins()
+        setCircle()
     }
     
     // MARK: - Initializing
     
     func configureMapView() {
         
-        mapView = MTMapView()
+        mapView = MTMapView.init(frame: mapView.frame)
         mapView.delegate = self
         mapView.baseMapType = .standard
 
     }
     
+    func setSomePins(){
+        poiItem1 = MTMapPOIItem()
+        poiItem1.itemName = "City on a Hill"
+        poiItem1.mapPoint = MTMapPoint(geoCoord: MTMapPointGeo(latitude: 37.541889, longitude: 127.095388))
+        poiItem1.showDisclosureButtonOnCalloutBalloon = true
+        poiItem1.markerType = .redPin
+        poiItem1.showAnimationType = .dropFromHeaven;
+        poiItem1.draggable = true
+        poiItem1.tag = 1;
+
+        mapView.add(poiItem1)
+        mapView.fitArea(toShowMapPoints: [poiItem1.mapPoint!])
+        
+        
+    }
+    
+    func setCircle(){
+        let circle = MTMapCircle()
+        circle.circleCenterPoint = MTMapPoint(geoCoord: MTMapPointGeo(latitude: 37.541889, longitude: 127.095388))
+        circle.circleLineColor = .red
+        circle.circleFillColor = UIColor.green.withAlphaComponent(0.5)
+        circle.tag = 1234
+        circle.circleRadius = 500
+        mapView.addCircle(circle)
+        mapView.fitArea(toShow: circle)
+    }
+    
     func bindViewModel() {
         
+    }
+    
+    func mapView(_ mapView: MTMapView!, openAPIKeyAuthenticationResultCode resultCode: Int32, resultMessage: String!) {
+        print(resultCode)
     }
 }
