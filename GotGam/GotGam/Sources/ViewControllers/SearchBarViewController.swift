9e1d0a699bdd4d6dc48cb0635dc8d264c61d31b2
bomin <kbm97121@gmail.com>
2020-05-07 22:51:08 +0900

    서치바 UI 구현 , 아이콘 추가

---
+++ GotGam/GotGam/Sources/ViewControllers/SearchBarViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SearchBarViewController.swift
+//  GotGam
+//
+//  Created by 김삼복 on 07/05/2020.
+//  Copyright © 2020 손병근. All rights reserved.
+//
+
+import Foundation
+
+class SearchBarViewController: UIViewController, ViewModelBindableType {
+	
+	var viewModel: SearchBarViewModel!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+	
+	func bindViewModel() {
+		<#code#>
+	}
+}
---
+++ GotGam/GotGam/Sources/ViewModels/SearchBarViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SearchBarViewModel.swift
+//  GotGam
+//
+//  Created by 김삼복 on 07/05/2020.
+//  Copyright © 2020 손병근. All rights reserved.
+//
+
+import Foundation
+
+class SearchBarViewModel: CommonViewModel {
+	
+}
