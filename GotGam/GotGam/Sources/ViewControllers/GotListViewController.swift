751bcff1e926f06f60f0febeca2e6a130a3feb9b
bomin <kbm97121@gmail.com>
2020-05-12 16:07:14 +0900

    List 네비게이션에 ID 추가함

--- GotGam/GotGam/Sources/ViewControllers/GotListViewController.swift
+++ GotGam/GotGam/Sources/ViewControllers/GotListViewController.swift
@@ -19,12 +19,14 @@ class GotListViewController: BaseViewController, ViewModelBindableType {
     var memos = [ManagedGot]()
     
     // MARK: - Views
-    @IBOutlet weak var gotListTableView: UITableView!
 
-
+	@IBOutlet weak var gotListTableView: UITableView!
+	
 	@IBOutlet var ListAddButton: UIButton!
+	
 	@IBAction func moveAddVC(_ sender: Any) {
 		viewModel.inputs.showVC()
+
 	}
 	// MARK: - View Life Cycle
     
@@ -60,12 +62,12 @@ class GotListViewController: BaseViewController, ViewModelBindableType {
     
     func bindViewModel() {
         
-        viewModel.outputs.gotList
-                  .bind(to: gotListTableView.rx.items(cellIdentifier: "gotListCell", cellType: UITableViewCell.self)) { index, got, cell in
-                      print(got.title)
-                cell.textLabel?.text = got.title
-            }
-            .disposed(by: disposeBag)
+//        viewModel.outputs.gotList
+//                  .bind(to: gotListTableView.rx.items(cellIdentifier: "gotListCell", cellType: UITableViewCell.self)) { index, got, cell in
+//                      print(got.title)
+//                cell.textLabel?.text = got.title
+//            }
+//            .disposed(by: disposeBag)
       
           
       }
