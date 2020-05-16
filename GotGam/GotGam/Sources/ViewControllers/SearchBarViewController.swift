b04fc26820ddcdeab3b9799adcfc35f9395d2575
bomin <kbm97121@gmail.com>
2020-05-16 18:01:59 +0900

    search 토요일 회의때한거

--- GotGam/GotGam/Sources/ViewControllers/SearchBarViewController.swift
+++ GotGam/GotGam/Sources/ViewControllers/SearchBarViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
-class SearchBarViewController: UIViewController, ViewModelBindableType {
+class SearchBarViewController: BaseViewController, ViewModelBindableType {
 	
 	var viewModel: SearchBarViewModel!
 	
@@ -16,13 +18,50 @@ class SearchBarViewController: UIViewController, ViewModelBindableType {
 	@IBAction func moveMap(_ sender: Any) {
 		self.dismiss(animated: true)
 	}
+	@IBOutlet weak var tableView: UITableView!
 	
+	var placeList: [Place] = [] {
+		didSet{
+			DispatchQueue.main.async {
+				self.tableView.reloadData()
+			}
+		}
+	}
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		SearchBar.becomeFirstResponder()
+		//SearchBar.becomeFirstResponder()
+		
+		SearchBar.rx.text.orEmpty.debounce(.seconds(2), scheduler: MainScheduler.instance)
+			.subscribe(onNext: { text in
+				self.searchKeyword(keyword: text)
+			}).disposed(by: self.disposeBag)
+		
 	}
 	
+	
 	func bindViewModel() {
+	}
+	
+	
+	func searchKeyword(keyword: String){
+		APIManager.shared.search(keyword: keyword) { placeList in
+			let place = placeList.first!
+			print(place.addressName)
+			self.placeList = placeList
+		}
+	}
+	
+}
+extension SearchBarViewController: UITableViewDataSource{
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return self.placeList.count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let place = placeList[indexPath.row]
 		
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+		cell.textLabel?.text = place.addressName
+		return cell
 	}
 }
